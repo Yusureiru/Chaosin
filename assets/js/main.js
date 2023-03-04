3ba99b4af39f31611b0f60b4d5e58eb094324b49
Yusril <erilmbamba@gmail.com>
2023-03-04 21:17:57 +0700

    Second Commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", this.window.screenY > 0)
+});
+
+let menu = document.querySelector("#icon-menu");
+let navmenu = document.querySelector(".nav-menu");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navmenu.classList.toggle("open");
+}
